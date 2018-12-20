34f3c337de5ee38cb340af14ee29f8492cd59ae4
ilabstaff <ilabstaff@172.16.116.68>
2018-12-19 17:54:24 -0800

    treasure Hunt

--- src/section4/RobotTreasureHunt.java
+++ src/section4/RobotTreasureHunt.java
@@ -16,42 +16,57 @@ import org.jointheleague.graphical.robot.Robot;
 public class RobotTreasureHunt implements KeyEventDispatcher{
 
 	// 1. Create a new mini robot (type "mini" inside the parentheses)
-	
+	Robot rob =new Robot("mini");
 	private void goUp() throws InterruptedException {
 		// 2. Make the robot move up the screen (use setAngle(angle) and microMove(distance))
-		
+		rob.setAngle(0);
+	rob.microMove(3);
+	
 	}
 
 	private void goDown() throws InterruptedException{
 		// 3. make the robot move down the screen (use setAngle(angle) and microMove(distance))
-		
+		rob.setAngle(180);
+	rob.microMove(3);
 	}
 
 	private void turnLeft() throws InterruptedException{
 		// 4. Make the robot turn to the left (use setAngle(angle) and microMove(distance))
-
+rob.setAngle(270);
+	rob.microMove(3);
 	}
 
 	private void turnRight() throws InterruptedException{
 		// 5. make the robot turn to the right (use setAngle(angle) and microMove(distance))
-		
+		rob.setAngle(90);
+	rob.microMove(3);
 	}
 
 	private void spaceBarWasPressed() {
 
 		// 5. Change ROBOTNAME below to match the name of the robot you created in step 1.  THEN, remove the slashes at the beginning of the next two lines
-		//int robotXLocation = ROBOTNAME.getX();
-		//int robotYLocation = ROBOTNAME.getY();
+		int robotXLocation = rob.getX();
+		int robotYLocation = rob.getY();
 		
 		// 6. Print the robotXLocation and robotYLocation variables to the console 
-		
+		System.out.println(robotXLocation + " " + robotYLocation);
 		// 7. If robot is at same location as the little girl
 		//      --make a pop-up tell the robot where to go next
-		
+		if(robotXLocation==720&&robotYLocation==405) {
+			JOptionPane.showMessageDialog(null,"Go to the skull");
+		}
 		// 8. Give the user subsequent clues at different locations on the image
 		// (pirate robot, swamp, parrots, etc.)
+		if(robotXLocation==255&&robotYLocation==117) {
+			JOptionPane.showMessageDialog(null,"Go to pirate robot");
 		
-		// 9.  If the robot is in the final location
+			
+		
+			if(robotXLocation==255&&robotYLocation==117) {
+				JOptionPane.showMessageDialog(null,"Go to pirate robot");
+			}
+		}
+			// 9.  If the robot is in the final location
 		//     --call the treasureFound() method
 		
 	}
